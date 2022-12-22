a6f6996c3615f6c3754c23f03910b90bc83d887e
Miha_GB <m-stogov@yandex.ru>
2022-12-22 22:06:41 +0700

    task 4

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
-
+/*
 Console.WriteLine("Введите первое число для сравнения");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число для сравнения");
@@ -15,3 +15,22 @@ if (a > b)
     }
 else
         Console.WriteLine($"{b} больше {a}");
+*/
+
+/*Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+
+int a = 2; int b = 3; int c = 7; int max = 0;
+
+if (a > b)
+    {max = a;}
+else
+    {max = b;}
+if (max > c)
+    {}
+else
+    {max = c;}
+Console.WriteLine(max);
