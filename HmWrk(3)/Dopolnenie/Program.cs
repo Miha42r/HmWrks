b1da50778ea32cb40eba99f7c8f4c0f7f145610c
Miha_GB <m-stogov@yandex.ru>
2022-12-25 06:27:58 +0700

    Simple Numbers

---
+++ HmWrk(3)/Dopolnenie/Program.cs
@@ -0,0 +1,20 @@
+﻿void SimpleNumber(long i)
+{
+    for (long index = 1; index <= i; index++)
+    {
+        if ((true) == (index % 2) > 0)
+        {
+        Console.WriteLine($"{index} число простое");
+        }
+    }
+
+}
+
+
+Console.WriteLine("Введите число ");
+long i = long.Parse(Console.ReadLine()!);
+SimpleNumber(i);
+
+
+
+
