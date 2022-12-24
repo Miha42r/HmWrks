3e077f55d50c3b2aba7f15f12c129ce0c35e1968
Miha_GB <m-stogov@yandex.ru>
2022-12-25 05:46:42 +0700

    ready cube

--- HmWrk(3)/task3_23/Program.cs
+++ HmWrk(3)/task3_23/Program.cs
@@ -1,7 +1,14 @@
-﻿
+﻿void Cube(long i)
+{
+    for (long index = 1; index <= i; index++)
+    {
+        long cub = index * index * index;
+        Console.WriteLine($"Куб числа {index} равен {cub}");
+    }
+
+}
 
 
-
-Console.WriteLine("Введите число");
-ulong N = ulong.Parse(Console.ReadLine()!);
-
+Console.WriteLine("Введите число ");
+long i = long.Parse(Console.ReadLine()!);
+Cube(i);
