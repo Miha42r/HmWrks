12dfbe48a8b6bb48e2800b0529408954aefc18fb
Miha_GB <m-stogov@yandex.ru>
2022-12-25 00:05:00 +0700

    Освоил Void

--- Task2/task 2_15/Program.cs
+++ Task2/task 2_15/Program.cs
@@ -1,12 +1,15 @@
-﻿
+﻿void Proverka(short n)
+{
+    if (n <= 5 & n >= 1)
+    {
+        Console.WriteLine("Будни");
+    }
+    else
+    {
+        if (n == 6 || n == 7) Console.WriteLine("Выходной");
+    }
+}
+
 Console.WriteLine("Введите номер дня недели от 1 до 7");
 short n = short.Parse(Console.ReadLine()!);
-
-if (n<=5 & n>=1)
-{
-    Console.WriteLine("Будни");
-}
-else
-{
-    if (n==6 || n == 7) Console.WriteLine("Выходной");    
-}
+Proverka(n);
