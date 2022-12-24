cbdd61380e6382f3d54830e6eaaf95800c519d26
Miha_GB <m-stogov@yandex.ru>
2022-12-24 22:42:01 +0700

    T1

---
+++ Task2/Proga2_13.cs
@@ -0,0 +1,40 @@
+﻿int Input(int str)
+{
+str = int.Parse(Console.ReadLine()!);
+return str;
+}
+
+int EjectThirdDigit(int x) //Извлекаем третий разряд
+{
+    Console.WriteLine("Извлекаем третий разряд");
+    int a = x % 10;
+    return a;
+}
+
+int DigitsNumber(int numb) // привожу число к трёхзначному
+{
+    Console.WriteLine("Приводим число к трехзначному");
+    while (numb > 999)
+    {
+        numb = numb / 10;
+    }
+    return numb;
+}
+
+Console.WriteLine("Введите число");
+int numb = Input(numb);
+if (numb < 99) //проверяем разрядность числа
+{
+    Console.WriteLine($"{numb} - у данного числа нет третьего разряда, введите число");
+}
+if (numb > 999)
+{
+    numb = DigitsNumber(numb);
+    Console.WriteLine(EjectThirdDigit(numb));
+}
+else
+{
+    Console.WriteLine(EjectThirdDigit(numb));
+}
+
+
---
+++ Task2/task 2_10/Progr2_10.cs
@@ -0,0 +1,13 @@
+﻿int EjectTwoDigit(int x) //Извлекаем второй разряд
+{
+    Console.WriteLine("Извлекаем второй разряд");
+    int a = (x / 10) % 10;
+    return a;
+}
+
+Console.WriteLine("Введите число");
+int numb = int.Parse(Console.ReadLine()!);
+if (numb < 999)
+{
+    Console.WriteLine(EjectTwoDigit(numb));
+}
