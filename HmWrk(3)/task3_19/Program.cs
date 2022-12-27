1fd0b17d1c90b83eaab42e5d20897b5a362e66be
Miha_GB <m-stogov@yandex.ru>
2022-12-27 15:16:25 +0700

    ready Palindrom

---
+++ HmWrk(3)/task3_19/Program.cs
@@ -0,0 +1,38 @@
+﻿// Программа поиска числа палиндром.
+
+void Palindrom(int x)
+{
+    string a = Convert.ToString(x);
+    int e = x % 10;
+    x = x / 10;
+    int d = x % 10;
+    x = x / 10;
+    int c = x % 10;
+    x = x / 10;
+    int b = x % 10;
+    x = x / 10;
+    
+    string x_mirror = string.Concat(e, d, c, b, x);
+    if (a == x_mirror)
+    {
+        Console.WriteLine($"{x_mirror} - число является палиндромом" );
+    }
+    else
+    {
+        Console.WriteLine($"{x_mirror} - число не является палиндромом" );
+    }
+    
+}
+
+Console.WriteLine("Введите пятизначное число");
+int x = int.Parse(Console.ReadLine()!);
+
+string str = Convert.ToString(x);
+if (str.Length == 5)
+{
+    Palindrom(x);
+}
+else
+{
+    Console.WriteLine("Введите пожалуйста ПЯТИзначное число");
+}
