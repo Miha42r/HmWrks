e6b1462b82fd198cb3b621b045a51c119feb403d
Miha_GB <m-stogov@yandex.ru>
2023-01-10 21:43:01 +0700

    Dopolnenie not work(

--- HmWrk(3)/Dopolnenie/Program.cs
+++ HmWrk(3)/Dopolnenie/Program.cs
@@ -1,20 +1,27 @@
-﻿void SimpleNumber(long i)
+﻿void SimpleNumber(int numb)
 {
-    for (long index = 1; index <= i; index++)
+    int[] arr = new int[numb];                  //создаем массив
+    for (int index = 0; index < numb; index++)
     {
-        if ((true) == (index % 2) > 0)
-        {
-        Console.WriteLine($"{index} число простое");
-        }
+        arr[index] = index + 1;
     }
 
+    for (int i = 1; i < numb; i++)
+    {
+        for(int j = 2; j <= numb; j++)
+        {
+        if (arr[i] % j != 0)
+            {
+            arr[i] = i;
+            }
+            Console.WriteLine(arr[i]);
+        }
+    
+    }
 }
 
 
 Console.WriteLine("Введите число ");
-long i = long.Parse(Console.ReadLine()!);
-SimpleNumber(i);
+int numb = int.Parse(Console.ReadLine()!);
 
-
-
-
+SimpleNumber(numb);
