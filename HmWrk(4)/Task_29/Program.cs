2f34b1b09aa6cec2e7dd43482d608f1214f8337e
Miha_GB <m-stogov@yandex.ru>
2023-01-14 22:45:02 +0700

    Task 29 ready

---
+++ HmWrk(4)/Task_29/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+int[] AddArray()
+{
+    int[] array = new int[8];
+    Random rnd = new Random();
+    int i = 0;
+    while (i < array.Length)
+    {
+        array[i] = rnd.Next(0, 99);
+        i++;
+    }
+return array;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var i in arr)
+    {
+        Console.Write(i + " ");
+    }
+}
+
+Console.WriteLine("Программа создает массив из 8 случайных элементов и выводит их на экран");
+
+int[] arr = AddArray();
+//Выводим массив одним из способов:
+PrintArray(arr);
+
+/*string s = string.Join(" ", arr);
+Console.WriteLine(s);
+*/
+
+
+ 
