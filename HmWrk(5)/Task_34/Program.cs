2ba0c5f704f2d90985935de1de997145f9db9f39
Miha_GB <m-stogov@yandex.ru>
2023-01-15 23:37:43 +0700

    Объединил задание 34 и 36(Task_34)

--- HmWrk(5)/Task_34/Program.cs
+++ HmWrk(5)/Task_34/Program.cs
@@ -41,7 +41,7 @@ return count;
 int SummaElementsEvenIndex(int[] array) //сумма элементов на четных позициях
 {
     int summ=0;
-    for (int i = 0; i < array.Length; i = i+2)
+    for (int i = 1; i < array.Length; i = i+2)
     {
         summ = array[i] + summ;
     }
