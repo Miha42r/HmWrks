a729cb29295009ef7da4cb12a731bb648047f554
Miha_GB <m-stogov@yandex.ru>
2023-01-25 22:45:31 +0700

    Task 41 in job

---
+++ HmWrk(6)/Task41/Program.cs
@@ -0,0 +1,40 @@
+﻿
+string[] AS;
+int count;
+string s;
+string[] AS2;
+
+Console.WriteLine("Введите числа, по окончанию ввода нажмите 'Enter'");
+
+count = 0; // обнулить количество строк
+AS = new string[count]; // выделить память для 0 строк
+
+do
+{
+    s = Console.ReadLine()!;
+    if (s != "")
+    {
+        count++;
+        AS2 = new string[count];
+
+        for (int i = 0; i < AS2.Length - 1; i++)
+            AS2[i] = AS[i];
+            AS2[count - 1] = s;
+
+        AS = AS2;
+    }
+} while (s != "");
+
+// 3. Вывод массива строк AS в цикле
+int p = 0;
+for (int i = 0; i < AS.Length; i++)
+{
+    
+    int d = Convert.ToInt32(AS[i]);
+    if (d > 0)
+    {
+        p++;
+    }
+
+}
+Console.WriteLine("Количество положительных = " + p);
