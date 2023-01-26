e78f63146b95d539d707050c860108674eddf164
Miha_GB <m-stogov@yandex.ru>
2023-01-26 21:59:52 +0700

    task 41 мой вариант

--- HmWrk(6)/Task41/Program.cs
+++ HmWrk(6)/Task41/Program.cs
@@ -1,40 +1,22 @@
 ﻿
-string[] AS;
-int count;
-string s;
-string[] AS2;
-
-Console.WriteLine("Введите числа, по окончанию ввода нажмите 'Enter'");
-
-count = 0; // обнулить количество строк
-AS = new string[count]; // выделить память для 0 строк
-
-do
+int QuantityPositiveNumb()
 {
-    s = Console.ReadLine()!;
-    if (s != "")
+    string s;
+    int count = 0;
+    do
     {
-        count++;
-        AS2 = new string[count];
+        s = Console.ReadLine()!;
+        if (s != "")
+        {
+            if (Convert.ToInt32(s) > 0)
+            {
+                count++;
+            }
+        }
 
-        for (int i = 0; i < AS2.Length - 1; i++)
-            AS2[i] = AS[i];
-            AS2[count - 1] = s;
+    } while (s != "");
 
-        AS = AS2;
-    }
-} while (s != "");
-
-// 3. Вывод массива строк AS в цикле
-int p = 0;
-for (int i = 0; i < AS.Length; i++)
-{
-    
-    int d = Convert.ToInt32(AS[i]);
-    if (d > 0)
-    {
-        p++;
-    }
-
+    return count;
 }
-Console.WriteLine("Количество положительных = " + p);
+Console.WriteLine("Введите числа, по окончанию ввода нажмите 'Enter'");
+Console.WriteLine("Введено положительных чисел: " + QuantityPositiveNumb());
