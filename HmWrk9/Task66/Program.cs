bfdf0427e74f1733e1da3a3f2db489c7a0677c3c
Miha_GB <m-stogov@yandex.ru>
2023-01-31 19:36:28 +0700

    task 66 ready

---
+++ HmWrk9/Task66/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int SummaNumbers(int m, int n, int i)
+{
+    if (n <= i) { return m; }
+    m = m + (SummaNumbers(m + 1, n - 1, i));
+    return m;
+}
+Console.WriteLine("Введите начало и окончание ряда чисел, от меньшего к большему");
+Console.WriteLine("Input start number");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Input end number");
+int n = int.Parse(Console.ReadLine()!);
+int i = m; //запоминаем переменную до которой необходимо посчитать
+Console.WriteLine($"the sum of the numbers in order = {SummaNumbers(m, n, i)}");
