105d86484eb59fa28368f812a9b3052d979d9ea3
Miha_GB <m-stogov@yandex.ru>
2023-01-31 20:49:42 +0700

    Add task 43(Прямые)(после разбора)

--- HmWrk(6)/Task43/Program.cs
+++ HmWrk(6)/Task43/Program.cs
@@ -1,24 +1,28 @@
 ﻿void PointOfIntersection(double k1, double b1, double k2, double b2)
 {
-    else if (k1 == k2){Console.WriteLine("Прямые параллельны");}
-    
     if (k1 == k2 && b1 == b2)
     {
         Console.WriteLine("Прямые совпадают, имеют множество точек пересечения");
+    }
+
+    else if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны");        
     }
     else
     {
         double x = (b1 - b2) / (k2 - k1);
         double y = k2 * x + b2;
-        Console.WriteLine
-        ($"Точка пересечения M, прямых A и B, имеет координаты ({Math.Round(x, 2)}, {Math.Round(y, 2)})");
+        Console.WriteLine($"Точка пересечения M, прямых A и B, имеет координаты ({Math.Round(x, 2)}, {Math.Round(y, 2)})");
     }
+
 }
 
 //Console.Write("Введите коэффициенты k1 и b1 прямой A");
 int k1 = 5;
 int b1 = 2;
+
 //Console.Write("Введите коэффициенты k2 и b2 прямой B");
-int k2 = 5;
+int k2 = 9;
 int b2 = 4;
 PointOfIntersection(k1, b1, k2, b2);
