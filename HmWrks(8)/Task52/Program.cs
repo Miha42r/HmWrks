5351e29bbd04ee88813c0fbedc51c72d3b79cd1a
Miha_GB <m-stogov@yandex.ru>
2023-02-02 22:14:01 +0700

    Task 54 ready(modify)

--- HmWrks(8)/Task52/Program.cs
+++ HmWrks(8)/Task52/Program.cs
@@ -23,22 +23,23 @@ void PrintArray(int[,] array)
 
 int[,] SortStringArray(int[,] arr)
 {
-    for (int i = 0; i < arr.GetLength(0); i++)
+    for (int a = 0; a < arr.Length; a++)
     {
-        for (int j = 0; j < arr.GetLength(1) - 1; j++)
+        for (int i = 0; i < arr.GetLength(0); i++)
         {
-            if (arr[i, j] < arr[i, j + 1])
+            for (int j = 0; j < arr.GetUpperBound(1); j++)
             {
-                int max = arr[i, j + 1];
-                arr[i, j + 1] = arr[i, j];
-                arr[i, j] = max;
+                if (arr[i, j] < arr[i, j + 1])
+                {
+                    int max = arr[i, j + 1];
+                    arr[i, j + 1] = arr[i, j];
+                    arr[i, j] = max;
+                }
             }
         }
     }
     return arr;
 }
 int[,] arr = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
-
-arr = SortStringArray(arr);
 arr = SortStringArray(arr);
 PrintArray(arr);
